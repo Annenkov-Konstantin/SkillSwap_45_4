bd4782c91e9a76e568d14a274dc0d70c25d93b3f
Nadezhda Martin <nadej-martin@yandex.ru>
2026-02-11 16:13:19 +0300

    feat: добавлена библиотека swiper, реализована галерея изображений со страницы навыков пользователя

--- src/widgets/ExampleComponent/exampleComponent.tsx
+++ src/widgets/ExampleComponent/exampleComponent.tsx
@@ -4,6 +4,7 @@ import styles from './exampleComponent.module.scss';
 import { Button, DropdownTrigger } from '@/shared/ui';
 import { Input } from '@/shared/ui';
 import { Checkbox } from '@/shared/ui/checkbox/checkbox';
+import { SkillGallery } from '@/features/SkillGallery';
 
 export const ExampleComponent: FC = () => {
   const [open, setOpen] = useState(false);
@@ -11,6 +12,16 @@ export const ExampleComponent: FC = () => {
   const [checked, setChecked] = useState(false);
   const [checked1, setChecked1] = useState(false);
   const [checked2, setChecked2] = useState(false);
+
+const images = [
+  "https://i.pinimg.com/1200x/9a/35/00/9a35001136e00e4f6ba6d16125077886.jpg",
+  "https://i.pinimg.com/736x/f8/b1/08/f8b108f138b66a1cb0ba23fe1855dc1f.jpg",
+  "https://i.pinimg.com/736x/27/0b/27/270b279865a032e1cff0e2adb6feed97.jpg",
+  "https://i.pinimg.com/736x/9d/2a/13/9d2a13cddf643a68f3523c3fc08cf6e5.jpg",
+  "https://i.pinimg.com/736x/50/14/47/5014470afdcf62d6824807ba22d22228.jpg"
+];
+
+const title = "Разработка веб-приложений на React";
 
   return (
     <div>
@@ -24,6 +35,7 @@ export const ExampleComponent: FC = () => {
       <Checkbox checked={checked} label='Бизнес и карьера' onChange={setChecked} />
       <Checkbox checked={checked1} label='Творчество и искусство' onChange={setChecked1} />
       <Checkbox checked={checked2} label='Спорт и здоровье' onChange={setChecked2} />
+      <SkillGallery images={images} title={title}/>
     </div>
   );
 };
