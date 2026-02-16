20f1ae9c5827d9ddc126672a52403066c20be669
Диана М <alisa.sokolova.2001@list.ru>
2026-02-16 23:42:52 +0300

    Реализация компонента FormStepCounter

---
+++ src/features/FormStepCounter/index.ts
@@ -0,0 +1,1 @@
+export { FormStepCounter } from './FormStepCounter';
--- src/features/index.ts
+++ src/features/index.ts
@@ -3,3 +3,4 @@ export { UserCardAvatar } from './UserCard/UserCardAvatar';
 export { SkillGallery } from './SkillGallery';
 export { UserAvatar } from './UserAvatar';
 export { UserCardSkillCounter } from './UserCard/UserCardSkillCounter';
+export { FormStepCounter } from './FormStepCounter';
