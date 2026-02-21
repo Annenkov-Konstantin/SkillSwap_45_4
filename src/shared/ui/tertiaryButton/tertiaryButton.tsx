e3cd299653c4ba573e65815bba3f504d7edc5707
Nadezhda Martin <nadej-martin@yandex.ru>
2026-02-21 17:54:11 +0300

    fix: добавлен обязательный пропс из-за стилей для label и иконки при клике и наведении

--- src/shared/ui/tertiaryButton/tertiaryButton.tsx
+++ src/shared/ui/tertiaryButton/tertiaryButton.tsx
@@ -7,13 +7,14 @@ export const TertiaryButton: React.FC<ITertiaryButton> = ({
   label,
   onClickButton,
   secondIcon,
+  hasIcons,
   ...rest
 }) => {
   const [isKeyPressed, setIsKeyPressed] = useState(false);
 
   if (!label) return null;
 
-  const hasIcons = Boolean(firstIcon || secondIcon);
+  // const hasIcons = Boolean(firstIcon || secondIcon);
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
     if (e.key === 'Enter') {
