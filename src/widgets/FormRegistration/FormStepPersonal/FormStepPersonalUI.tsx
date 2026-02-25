4b01c787330fd02c8f73756e593932d4aaa78d6b
Nadezhda Martin <nadej-martin@yandex.ru>
2026-02-25 11:20:05 +0300

    feat: логика для 2-го шага регистрации

--- src/widgets/FormRegistration/FormStepPersonal/FormStepPersonalUI.tsx
+++ src/widgets/FormRegistration/FormStepPersonal/FormStepPersonalUI.tsx
@@ -1,16 +1,13 @@
-import type { FC } from 'react';
-
+import { type FC } from 'react';
 import { Input } from '@/shared/ui';
 import { Select } from '@/shared/ui';
 import { CitySelect } from '@/features';
 import { SkillSelect } from '@/features/SkillSelect';
 import { Button } from '@/shared/ui';
-import { Icon } from '@/shared/ui/Icon';
-
+import { CalendarInput } from '@/shared/ui/dateInputCalendar';
+import styles from './formStepPersonal.module.scss';
 import { optionsGender } from './types';
 import type { FormStepPersonalUIProps } from './types';
-
-import styles from './formStepPersonal.module.scss';
 
 export const FormStepPersonalUI: FC<FormStepPersonalUIProps> = ({
   nameValue,
@@ -19,11 +16,21 @@ export const FormStepPersonalUI: FC<FormStepPersonalUIProps> = ({
   categoryArray,
   cityArray,
   genderValue,
+  cityValue,
+  selectedCategory,
+  selectedSkill,
   handleSubmit,
   profilePhotoAdd,
   onNameChange,
   onBirthChange,
-  onGenderChange
+  onGenderChange,
+  onCityChange,
+  onCategoryChange,
+  onSkillChange,
+  onForwardClick,
+  onBackClick,
+  showNameError,
+  getValueHint
 }: FormStepPersonalUIProps) => {
   return (
     <form className={styles.formContainer} onSubmit={handleSubmit}>
@@ -75,39 +82,56 @@ export const FormStepPersonalUI: FC<FormStepPersonalUIProps> = ({
           value={nameValue}
           onChange={onNameChange}
           name='userName'
-          className={styles.nameInput}
-        ></Input>
+          className={`${styles.nameInput} ${showNameError ? styles.inputError : ''}`}
+        />
+        {getValueHint && getValueHint()}
       </div>
       <div className={styles.personalDataContainer}>
         <div className={styles.dateBirthContainer}>
           <label htmlFor='dateBirth'>Дата рождения</label>
-          <Input
-            placeholder='дд.мм.гггг'
+          <CalendarInput
             value={birthValue}
             onChange={onBirthChange}
             name='dateBirth'
-          ></Input>
+          />
         </div>
         <Select label='Пол' options={optionsGender} value={genderValue} onChange={onGenderChange}></Select>
       </div>
-      <CitySelect cityList={cityArray}></CitySelect>
+      <CitySelect
+        cityList={cityArray}
+        value={cityValue}
+        onChange={onCityChange}
+      />
       <div className={styles.skillContainer}>
         <label>Категория навыка, которому хотите научиться</label>
         <SkillSelect
           placeholderValue='Выберите категорию'
           optionsArr={categoryArray}
-        ></SkillSelect>
+          value={selectedCategory}
+          onChange={onCategoryChange}
+        />
       </div>
       <div className={styles.skillContainer}>
         <label>Подкатегория навыка, которому хотите научиться</label>
         <SkillSelect
-          placeholderValue='Выберите подкатегорию'
+          placeholderValue={selectedCategory ? 'Выберите подкатегорию' : 'Сначала выберите категорию'}
           optionsArr={skillArray}
-        ></SkillSelect>
+          value={selectedSkill}
+          onChange={onSkillChange}
+          disabled={!selectedCategory}
+        />
       </div>
       <div className={styles.buttonContainer}>
-        <Button status='secondary' children='Назад'></Button>
-        <Button status='primary' children='Продолжить'></Button>
+        <Button
+          status='secondary'
+          children='Назад'
+          onClick={onForwardClick}
+        />
+        <Button
+          status='primary'
+          children='Продолжить'
+          onClick={onBackClick}
+        />
       </div>
     </form>
   );
--- src/widgets/FormRegistration/FormStepPersonal/types.ts
+++ src/widgets/FormRegistration/FormStepPersonal/types.ts
@@ -1,5 +1,6 @@
 import type React from 'react';
 import type { TCity } from '@/entities/city';
+import type { ReactNode } from 'react';
 
 export const optionsGender = [
   { label: 'Не имеет значения', value: 'any' },
@@ -15,10 +16,20 @@ export type FormStepPersonalUIProps = {
   categoryArray: string[];
   cityArray: TCity[];
   genderValue: string;
+  selectedCategory: string | null;
+  selectedSkill: string | null;
+  cityValue: string | null;
 
   profilePhotoAdd: () => void;
   handleSubmit: (e: React.SyntheticEvent<HTMLFormElement>) => void;
   onNameChange: (value: string)  => void;
   onBirthChange: (value: string)  => void;
-  onGenderChange: (value: string) => void
+  onGenderChange: (value: string) => void;
+  onCityChange: (city: string | null) => void;
+  onCategoryChange: (value: string) => void;
+  onSkillChange: (value: string) => void;
+  onForwardClick: () => void;
+  onBackClick: () => void;
+  showNameError: boolean;
+  getValueHint: () => ReactNode;
 };
