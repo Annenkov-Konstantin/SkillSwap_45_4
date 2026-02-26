986fb2c20b88fb1f02f6384980b898e22e58775a
Диана М <alisa.sokolova.2001@list.ru>
2026-02-26 22:06:08 +0300

    Фикс иконки аватарки на 2 шаге

--- src/pages/FormRegistration/FormStepPersonal/FormStepPersonalUI.tsx
+++ src/pages/FormRegistration/FormStepPersonal/FormStepPersonalUI.tsx
@@ -68,7 +68,7 @@ const handleMultiSkillChange = (ids: number[]) => {
             height={72}
           />
         ) : (
-          <Icon name='icon-user-circle' size={72} fill='none' />
+          <Icon name='icon-user-circle' size={72} fill='none' stroke='currentColor' />
         )}
         <button
           type='button'
